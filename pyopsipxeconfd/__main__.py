365b8b34f40d5c90671ac99c48bec55b3a708a73
Fabian Kalweit <f.kalweit@uib.de>
2020-05-28 14:27:17 +0200

    poetry init

---
+++ pyopsipxeconfd/__main__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+:copyright: uib GmbH <info@uib.de>
+This file is part of opsi - https://www.opsi.org
+
+:license: GNU Affero General Public License version 3
+See LICENSES/README.md for more Information
+"""
+
+from .opsipxeconfd import main
+
+if __name__ == '__main__':
+	main()
