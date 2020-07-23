442b50e870c0901e15ec85d3309ecccd4dd79bf3
doerrer <n.doerrer@uib.de>
2020-07-23 17:49:28 +0200

    [chg] disabled pytest log capture

--- tests/test_opsipxeconfd.py
+++ tests/test_opsipxeconfd.py
@@ -12,12 +12,6 @@ from opsipxeconfd.opsipxeconfd import OpsipxeconfdInit
 
 CONFFILE = "tests/test_data/opsipxeconfd.conf"
 
-def test_help_and_version():
-	opts = argparse.Namespace(help=True, version=None, command=None, conffile=None, logLevel=7, nofork=None)
-	OpsipxeconfdInit(opts)
-	opts = argparse.Namespace(help=None, version=True, command=None, conffile=None, logLevel=7, nofork=None)
-	OpsipxeconfdInit(opts)
-
 def test_OpsipxeconfdInit():
 	opts = argparse.Namespace(help=None, version=None, command="start", conffile=None, logLevel=7, nofork=True)
 	OpsipxeconfdInit(opts)
