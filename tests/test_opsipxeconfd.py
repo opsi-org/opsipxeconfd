ee900a4f5d0368e1279a5982da534447bcec3c6e
doerrer <n.doerrer@uib.de>
2020-07-23 17:16:10 +0200

    [fix] fixed the stop command injection

--- tests/test_opsipxeconfd.py
+++ tests/test_opsipxeconfd.py
@@ -12,6 +12,12 @@ from opsipxeconfd.opsipxeconfd import OpsipxeconfdInit
 
 CONFFILE = "tests/test_data/opsipxeconfd.conf"
 
+def test_help_and_version():
+	opts = argparse.Namespace(help=True, version=None, command=None, conffile=None, logLevel=7, nofork=None)
+	OpsipxeconfdInit(opts)
+	opts = argparse.Namespace(help=None, version=True, command=None, conffile=None, logLevel=7, nofork=None)
+	OpsipxeconfdInit(opts)
+
 def test_OpsipxeconfdInit():
 	opts = argparse.Namespace(help=None, version=None, command="start", conffile=None, logLevel=7, nofork=None)
 	OpsipxeconfdInit(opts)
@@ -19,5 +25,5 @@ def test_OpsipxeconfdInit():
 	opts = argparse.Namespace(help=None, version=None, command="status", conffile=None, logLevel=7, nofork=None)
 	OpsipxeconfdInit(opts)
 	time.sleep(3)
-	opts = argparse.Namespace(help=None, version=None, command="status", conffile=None, logLevel=7, nofork=None)
+	opts = argparse.Namespace(help=None, version=None, command="stop", conffile=None, logLevel=7, nofork=None)
 	OpsipxeconfdInit(opts)
