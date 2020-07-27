64dbb3b950fade018032ff8213b3de19e713fc0f
doerrer <n.doerrer@uib.de>
2020-07-27 10:18:32 +0200

    [fix] adapted tests to new argparse namespace

--- tests/test_opsipxeconfd.py
+++ tests/test_opsipxeconfd.py
@@ -10,12 +10,29 @@ import time
 from opsicommon.logging import LOG_WARNING
 from opsipxeconfd.opsipxeconfd import OpsipxeconfdInit
 
+default_opts = argparse.Namespace(	help=None,
+									version=None,
+									nofork=None,
+									conffile=None,
+									setup=None,
+									command="start",
+									logLevel=7,
+									logFile="/var/log/opsi/opsipxeconfd.log",
+									maxLogSize=5.0,
+									keepRotatedLogs=1,
+									logLevelFile=4,
+									logLevelStderr=7,
+									logFilter=None
+)
+
 def test_OpsipxeconfdInit():
 	#opts = argparse.Namespace(help=None, version=None, command="start", conffile=None, logLevel=7, nofork=None, setup=None)
 	#OpsipxeconfdInit(opts)
 	#time.sleep(12)
-	opts = argparse.Namespace(help=None, version=None, command="status", conffile=None, logLevel=7, nofork=None, setup=None)
+	opts = argparse.Namespace(**vars(default_opts))
+	opts.command = "status"
 	OpsipxeconfdInit(opts)
 	time.sleep(3)
-	opts = argparse.Namespace(help=None, version=None, command="stop", conffile=None, logLevel=7, nofork=None, setup=None)
+	opts = argparse.Namespace(**vars(default_opts))
+	opts.command = "stop"
 	OpsipxeconfdInit(opts)
