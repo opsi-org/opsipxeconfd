60ebddac6083ef8830bd29b27eb8cb3eccffb5e3
doerrer <n.doerrer@uib.de>
2020-07-29 16:05:57 +0200

    [new] added docstrings and type hints for logging and main

--- opsipxeconfd/logging.py
+++ opsipxeconfd/logging.py
@@ -10,21 +10,36 @@ import traceback
 import time
 import sys
 import os
+from typing import Dict
+import logging
 
 import opsicommon.logging
-import logging
 from opsicommon.logging import logger
 from logging.handlers import WatchedFileHandler, RotatingFileHandler
-import colorlog
 
-def init_logging(config):
+def init_logging(config : Dict) -> None:
+	"""
+	Initializes logging for opsipxeconfd.
+
+	This method takes an opsipxeconfd configuration dictionary and initializes
+	logging accordingly. A rotating file handler with appropriate settings
+	is added to the logger. If the opsipxeconfd is not started as daemon, a handler
+	for stderr is also added.
+
+	:param config: opsipxeconfd configuration dictionary as created by opsipxeconfdinit.
+	:type config: Dict
+	"""
 	try:
 		logLevel = max(config.get("logLevel"), config.get("logLevel_stderr"), config.get("logLevel_file"))
 		logLevel = logging._opsiLevelToLevel[logLevel]
 		if config['logFile']:
 			plain_formatter = logging.Formatter(opsicommon.logging.DEFAULT_FORMAT)
 			formatter = opsicommon.logging.ContextSecretFormatter(plain_formatter)
-			file_handler = RotatingFileHandler(config['logFile'], maxBytes=config['maxBytesLog'], backupCount=config['backupCountLog'])
+			file_handler = RotatingFileHandler(
+						config['logFile'],
+						maxBytes=config['maxBytesLog'],
+						backupCount=config['backupCountLog']
+			)
 			file_handler.setFormatter(formatter)
 			file_handler.setLevel(logging._opsiLevelToLevel[config.get("logLevel_file")])
 			logger.addHandler(file_handler)
