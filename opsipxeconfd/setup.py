7bad8346f3639b9d09a57d6695e915292309a4e5
Mathias Radtke <m.radtke@uib.de>
2023-05-22 16:04:20 +0200

    added code to add password hash

--- opsipxeconfd/setup.py
+++ opsipxeconfd/setup.py
@@ -21,10 +21,17 @@ from opsicommon.server.setup import setup_users_and_groups as po_setup_users_and
 
 logger = get_logger()
 
+def encodedPassword(clearPassword):
+	while True:
+		pwhash = passlib.hash.sha512_crypt.using(rounds=5000).hash(clearPassword)
+		if not pwhash or "." in pwhash:
+			print("Invalid hash, retrying")
+		else:
+			return pwhash
 
 def patchMenuFile(config: dict) -> None:
 	"""
-	Patch the address to the `configServer` into `menufile`.
+	Patch the address to the `configServer` and a password hash into `menufile`.
 
 	To find out where to patch we look for lines that starts with the
 	given `searchString` (excluding preceding whitespace).
@@ -32,6 +39,7 @@ def patchMenuFile(config: dict) -> None:
 	"""
 
 	service: ServiceClient = get_service_client()
+	appendParameter: str | None = ""
 	configserverId: str | None = ""
 	try:
 		configs = service.jsonrpc("host_getObjects", params=[[], {"type": "OpsiConfigserver"}])[0]
@@ -45,6 +53,10 @@ def patchMenuFile(config: dict) -> None:
 			raise RuntimeError(f"Failed to get config server address for {configserverUrl!r}")
 		if not configserverUrl.endswith("/rpc"):
 			configserverUrl += "/rpc"
+
+		appendConfigs = service.jsonrpc("config_getObjects", params=[[], {"id": "opsi-linux-bootimage.append"}])[0]
+		defaultAppendParams = appendConfigs.defaultValues
+
 	except OpsiServiceConnectionError:
 		pass
 	finally:
@@ -56,8 +68,8 @@ def patchMenuFile(config: dict) -> None:
 				for line in readMenu:
 					if line.strip().startswith("linux"):
 						if "service=" in line:
-							line = re.sub(r"service=\S+", "", line.rstrip())
-						newlines.append(f"{line.rstrip()} service={configserverUrl}\n")
+							line = re.sub(r"\s?service=\S+", "", line)
+						newlines.append(line.replace("console=ttyS0", "console=ttyS0 service=" + configserverUrl))
 						continue
 
 					newlines.append(line)
@@ -68,7 +80,32 @@ def patchMenuFile(config: dict) -> None:
 			logger.error("%r/grub.cfg not found", config["pxeDir"])
 	else:
 		logger.error("configserver URL not found for %r", configserverUrl)
-
+	
+	if defaultAppendParams:
+		for element in defaultAppendParams:
+			if "bootimageRootPassword" in element:
+				clearRootPassword = element.split("=")[1]
+				endcodedRootPassword = encodedPassword(clearRootPassword)
+				pwhEntry = f"pwh={endcodedRootPassword}"
+			if "pwh=" in element:
+				pwhEntry = element
+			if pwhEntry:
+				with open(config["pxeDir"] + "/grub.cfg", "r", encoding="utf-8") as readMenu:
+					for line in readMenu:
+						if line.strip().startswith("linux"):
+							if "pwh=" in line:
+								line = re.sub(r"\s?pwh=\S+", "", line)
+							newlines.append(line.replace("console=ttyS0", "console=ttyS0 " + pwhEntry))
+							continue
+						
+						newlines.append(line)
+				
+				with open(config["pxeDir"] + "/grub.cfg", "w", encoding="utf-8") as writeMenu:
+					writeMenu.writelines(newlines)
+			except FileNotFoundError:
+				logger.error("%r/grub.cfg not found", config["pxeDir"])
+		else:
+			logger.error("failed to add password hash to grub.cfg")
 
 def setup_files(log_file: str) -> None:
 	"""
