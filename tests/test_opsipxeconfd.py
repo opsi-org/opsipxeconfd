755d4362421100928e147723451985f865e6757d
Mathias Radtke <m.radtke@uib.de>
2023-07-04 13:59:47 +0200

    added new assert

--- tests/test_opsipxeconfd.py
+++ tests/test_opsipxeconfd.py
@@ -98,7 +98,8 @@ def test_patch_menu_file() -> None:
 		content = filehandle.read()
 		print(content)
 	assert 'service' in content
-
+	assert 'pwh' in content
+	
 def test_pid_file() -> None:
 	if os.path.exists(PID_FILE):
 		os.remove(PID_FILE)
