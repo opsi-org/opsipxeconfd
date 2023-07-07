7d03c3607105c646c221982a994984c66871b280
Mathias Radtke <m.radtke@uib.de>
2023-07-07 08:21:33 +0200

    some more debugging

--- opsipxeconfd/setup.py
+++ opsipxeconfd/setup.py
@@ -89,6 +89,8 @@ def patchMenuFile(config: dict) -> None:
 				for line in readMenu:
 					if line.strip().startswith("linux"):
 						print(line)
+						if linuxDefaultDict:
+							print(f"linuxDefaultDict before: {linuxDefaultDict}")
 						linuxAppendDict.clear()
 						if not linuxDefaultDict:
 							for element in line.split(" "):
@@ -96,20 +98,24 @@ def patchMenuFile(config: dict) -> None:
 									linuxDefaultDict[element.split("=")[0]] = element.split("=")[1]
 								else:
 									linuxDefaultDict[element] = ""
+						print(f"linuxDefaultDict after: {linuxDefaultDict}")
 						if "pwh" in linuxDefaultDict:
 							linuxDefaultDict.pop("pwh")
 						if "service" in linuxDefaultDict:
 							linuxDefaultDict.pop("service")
+						print(f"linuxDefaultDict after pop: {linuxDefaultDict}")
 						linuxNewlinesDict = linuxDefaultDict.copy()
 						for element in line.split(" "):
 							if "=" in element:
 								linuxAppendDict[element.split("=")[0]] = element.split("=")[1]
 							else:
 								linuxAppendDict[element] = ""
+						print(f"linuxAppendDict after: {linuxAppendDict}")
 						if "pwh" in linuxAppendDict:
 							linuxAppendDict.pop("pwh")
 						if "service" in linuxAppendDict:
 							linuxAppendDict.pop("service")
+						print(f"linuxAppendDict after pop: {linuxAppendDict}")
 						if pwhEntry:
 							linuxNewlinesDict[pwhEntry.split("=")[0]] = pwhEntry.split("=")[1]
 						if configserverUrl:
@@ -126,7 +132,7 @@ def patchMenuFile(config: dict) -> None:
 							else:
 								line += key + ' '
 						line = line + '\n'
-						print(line)
+						print(f"line: {line}")
 
 					newlines.append(line)
 
