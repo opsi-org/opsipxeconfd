b9b2fd3f0734c15dd450b91249a43fe945e7f1bd
Mathias Radtke <m.radtke@uib.de>
2024-09-26 11:58:11 +0200

    [chg] some prints for debugging

--- opsipxeconfd/setup.py
+++ opsipxeconfd/setup.py
@@ -230,13 +230,21 @@ def patchMenuFile(config: dict) -> None:
 							line = " ".join(k if v is None else f"{k}={v}" for k, v in linuxNewlinesDict.items()) + "\n"
 
 						elif line.strip().startswith("set passwordhash"):
+							print("set passwordhash")
 							if pwhEntry:
+								print("pwhEntry found")
 								passwordhash = pwhEntry.split("=", maxsplit=1)[1].replace("$", r"\$")
+								print(passwordhash)
 								line = f'set passwordhash="{passwordhash}"\n'
+								print(line)
 						elif line.strip().startswith("set language"):
+							print("set language")
 							if langEntry:
+								print("langEntry found")
 								language = langEntry.split("=")[1].strip(" \n\r")
+								print(language)
 								line = f'set language="{language}"\n'
+								print(line)
 
 						newlines.append(line)
 
