72aa2d918f1c4ed7c42e307db7b53f9bda43bef8
Jan Schneider <j.schneider@uib.de>
2025-09-18 08:16:50 +0200

    [fix] Remove duplicate method in MockServiceClient

--- tests/test_pxeconfigwriter.py
+++ tests/test_pxeconfigwriter.py
@@ -144,15 +144,6 @@ def test_pxe_config_oneTimePassword(tmp_path: Path) -> None:
 				}
 			}
 
-		def productPropertyState_getValues(
-			self,
-			product_ids: list[str] | str | None = None,
-			property_ids: list[str] | str | None = None,
-			object_ids: list[str] | str | None = None,
-			with_defaults: bool = True,
-		) -> dict[str, dict[str, dict[str, list[str] | list[bool]]]]:
-			return {}
-
 	mock_service_client = MockServiceClient()
 
 	with (
